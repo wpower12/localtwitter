520c4478117ef5a200b1e7bac8c51037c4cbceb4
wpower12 <willpowe@gmail.com>
2021-10-30 15:56:27 -0400

    working db creation method.

--- localtwitter/db.py
+++ localtwitter/db.py
@@ -1,26 +1,107 @@
 from datetime import datetime
 
+TABLES = [
+	["user", 
+	"""
+		CREATE TABLE `user` (
+		  `id` bigint NOT NULL,
+		  `name` varchar(45) NOT NULL,
+		  `screen_name` varchar(45) NOT NULL,
+		  `location` varchar(45) NOT NULL,
+		  `followers_count` varchar(45) NOT NULL,
+		  `created_at` datetime NOT NULL,
+		  `statuses_count` int NOT NULL,
+		  PRIMARY KEY (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+	"""],
+	["tweet", 
+	"""
+		CREATE TABLE `tweet` (
+		  `id` bigint NOT NULL,
+		  `userid` bigint NOT NULL,
+		  `created_at` datetime NOT NULL,
+		  `text` varchar(140) NOT NULL,
+		  PRIMARY KEY (`id`),
+		  KEY `user` (`userid`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+	"""],
+	["hashtag", 
+	"""
+		CREATE TABLE `hashtag` (
+		  `text` varchar(45) NOT NULL,
+		  PRIMARY KEY (`text`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
-def createTables(cnx):
+	"""],
+	["tweethashtag", 
+	"""
+		CREATE TABLE `tweethashtags` (
+		  `tweetid` bigint NOT NULL,
+		  `hashtag` varchar(45) NOT NULL,
+		  PRIMARY KEY (`tweetid`),
+		  UNIQUE KEY `TH_UNIQUE` (`hashtag`,`tweetid`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"""]]
 
-	pass
+CREATE_FKS = [
+	["tweet", 
+	 ["""
+		ALTER TABLE `tweet` 
+		ADD CONSTRAINT `user_fk`
+		  FOREIGN KEY (`userid`)
+		  REFERENCES `user` (`id`)
+		  ON DELETE NO ACTION
+		  ON UPDATE NO ACTION;
+		"""]],
+	["tweethashtag", 
+	 ["""
+		ALTER TABLE `tweethashtags`
+		ADD CONSTRAINT `fk_hashtext` 
+		  FOREIGN KEY (`hashtag`) 
+		  REFERENCES `hashtag` (`text`)
+		  ON DELETE NO ACTION
+		  ON UPDATE NO ACTION;
+	  """,
+	  """
+		ALTER TABLE `tweethashtags`
+		ADD CONSTRAINT `fk_tweetid`
+		  FOREIGN KEY (`tweetid`) 
+		  REFERENCES `tweet` (`id`)
+		  ON DELETE NO ACTION
+		  ON UPDATE NO ACTION;
+	  """]]]
+				
+def createSchema(cnx, db_name):
+	cur = cnx.cursor()
+
+	cur.execute("CREATE DATABASE {};".format(db_name))
+	cnx.database = db_name
+	print("created db {}".format(db_name))
+
+	for table, create_sql in TABLES:
+		cur.execute(create_sql)
+		print("created table {}".format(table))
+
+	for table, fks in CREATE_FKS:
+		for fk_sql in fks:
+			cur.execute(fk_sql)
+		print("created fk's for {}".format(table))
 
 
 def storeTweet(cnx, tweet):
-	insert_user = ("INSERT IGNORE INTO `localized_twitter`.`user` "
+	insert_user = ("INSERT IGNORE INTO `user` "
 		"(`id`,`name`,`screen_name`,`location`,`followers_count`,`created_at`, `statuses_count`) "
 		"VALUES "
 		"(%(id)s, %(name)s, %(screen_name)s, %(location)s, %(followers_count)s, %(created_at)s, %(statuses_count)s)")
 
-	insert_tweet = ("INSERT IGNORE INTO `localized_twitter`.`tweet` "
+	insert_tweet = ("INSERT IGNORE INTO `tweet` "
 		"(`id`,`userid`,`created_at`,`text`) "
 		"VALUES (%(id)s, %(userid)s, %(created_at)s, %(text)s)")
 
-	insert_hash = ("INSERT IGNORE INTO `localized_twitter`.`hashtag` "
+	insert_hash = ("INSERT IGNORE INTO `hashtag` "
 		"(`text`)"
 		"VALUES (%(text)s)")
 
-	insert_tweethash = ("INSERT IGNORE INTO `localized_twitter`.`tweethashtags` "
+	insert_tweethash = ("INSERT IGNORE INTO `tweethashtags` "
 		"(`tweetid`, `hashtag`)"
 		"VALUES (%(tweetid)s, %(hashtag)s)")
 
