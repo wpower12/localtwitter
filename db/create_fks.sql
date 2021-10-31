e55120ce09fc9b1d17a34efac88ee34bfafb2a34
wpower12 <willpowe@gmail.com>
2021-10-30 23:24:39 -0400

    cleaning up old sql files.

--- db/create_fks.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `localized_twitter`.`tweet` 
-ADD CONSTRAINT `user_fk`
-  FOREIGN KEY (`userid`)
-  REFERENCES `localized_twitter`.`user` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
