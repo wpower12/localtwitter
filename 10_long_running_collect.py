bb7018fb4816f133c459280013470f83233faee0
wpower12 <willpowe@gmail.com>
2021-11-01 21:57:49 -0400

    draft script to run on school server.

---
+++ 10_long_running_collect.py
@@ -0,0 +1,29 @@
+import mysql.connector
+import pandas as pd
+import tweepy
+from decouple import config
+
+import localtwitter
+
+DATABASE_NAME  = "Full_Run_DB_00"
+COUNTY_INFO_FN = "data/us-county-data.csv"
+
+# DB Connection
+cnx = mysql.connector.connect(user=config('DB_USER'),
+					password=config('DB_PASSWORD'),
+					host=config('DB_HOST'))
+
+localtwitter.createSchema(cnx, DATABASE_NAME)
+localtwitter.populateCountyTable(cnx, pd.read_csv(COUNTY_INFO_FN, dtype={'FIPS': str}))
+
+print("search and insert.")
+
+# Twitter API Connection
+auth = tweepy.OAuthHandler(config('T_CONSUME_KEY'), config('T_CONSUME_SECRET'))
+auth.set_access_token(config('T_ACCESS_KEY'), config('T_ACCESS_SECRET'))
+api = tweepy.API(auth)
+
+
+for i in range(1, 5):
+	print("running full collect, iteration: {}".format(i))
+	localtwitter.allCountySearchAndInsert(cnx, api, report=True, limit=100*i, distance="5km")
