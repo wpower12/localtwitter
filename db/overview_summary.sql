b6350f5a5e07ea632ac0969b65e3ac6b56f21955
wpower12 <willpowe@gmail.com>
2021-11-17 15:32:33 -0500

    added count of counties with X tweets to summary sql examples.

--- db/overview_summary.sql
+++ db/overview_summary.sql
@@ -3,4 +3,9 @@ SELECT
 (SELECT COUNT(*) FROM tweet) as tweets,
 (SELECT COUNT(*) FROM hashtag) as hashtags,
 (SELECT COUNT(*) FROM url) as urls,
-(SELECT COUNT(*) FROM county WHERE ignore_county=1) as ignored_counties;
+(SELECT COUNT(*) FROM county WHERE ignore_county=1) as ignored_counties
+(SELECT COUNT(numtweets) FROM
+	(SELECT fips, COUNT(*) as 'numtweets' FROM county 
+		JOIN tweet ON tweet.countyfips = county.fips
+	GROUP BY county.fips) AS countytotals
+WHERE numtweets > 100) as 'counties over 100';
