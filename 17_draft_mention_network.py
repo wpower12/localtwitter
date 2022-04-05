ee59dc20729369f1c1e28f1da7f597d4e5bfa658
wpower12 <willpowe@gmail.com>
2022-04-05 11:39:08 -0400

    mention network with fips.

--- 17_draft_mention_network.py
+++ 17_draft_mention_network.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import networkx as nx
 
+import localtwitter
+
 EDGES_FN = "data/full_mention_edges.csv"
 edges_df = pd.read_csv(EDGES_FN)
+
+USER_FIPS_FN = "data/users_with_fips_2022_02_28.csv"
+user_fips = localtwitter.read_csv_to_dict(USER_FIPS_FN)
 
 # source_user,mentioned_user,weight
 g = nx.from_pandas_edgelist(edges_df, 
@@ -10,6 +15,8 @@ g = nx.from_pandas_edgelist(edges_df,
 	target='mentioned_user',
 	edge_attr=True,
 	create_using=nx.DiGraph)
+
+nx.set_node_attributes(g, user_fips)
 
 print(g)
 
