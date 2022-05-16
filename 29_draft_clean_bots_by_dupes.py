3e05a06514027fd1372d75cc3931871d396f4039
wkp3 <willpowe@gmail.com>
2022-05-16 16:06:30 -0400

    draft - fixed issues with bot. ready for lab machine.

--- 29_draft_clean_bots_by_dupes.py
+++ 29_draft_clean_bots_by_dupes.py
@@ -1,3 +1,5 @@
+import time
+
 import mysql.connector
 from decouple import config
 
@@ -8,13 +10,18 @@ cnx = mysql.connector.connect(user=config('DB_USER'),
 					host=config('DB_HOST'),
 					database=DATABASE_NAME)
 
-COL_NAME = "dupe_ratio_02"
-BATCH_SIZE = 1000
+CREATE_COL = True
+COL_NAME = "dupe_ratio_05"
+BATCH_SIZE = 100
 
 ADD_COL_SQL = """
 	ALTER TABLE `user` 
 	ADD COLUMN `{}` DOUBLE NULL DEFAULT NULL;
 """.format(COL_NAME)
+
+COUNT_USERS = """
+	SELECT COUNT(*) FROM `user` WHERE {} is NULL;
+"""
 
 GET_USERS_SQL = """
 	SELECT id FROM `user` WHERE {} is NULL LIMIT {};
@@ -41,21 +48,27 @@ UPDATE_USER = """
 
 # Create the column for the result ratio.
 cur = cnx.cursor()
-cur.execute(ADD_COL_SQL)
-cnx.commit()
-cur.close()
 
-i = 0
+if CREATE_COL:
+	cur.execute(ADD_COL_SQL)
+	cnx.commit()
+
+cur.execute(COUNT_USERS.format(COL_NAME))
+total_users = cur.fetchone()[0]
+
+users_processed = 0
 while True:
-	cur = cnx.cursor()
+	batch_start = time.perf_counter()
 	cur.execute(GET_USERS_SQL)
 	users = cur.fetchall()
+	users_processed += len(users)
+	if len(users) == 0:
+		break
 
 	for user in users:
 		user_id = user[0]
 		cur.execute(COUNT_USER_TWEETS.format(user_id))
 		count = cur.fetchone()[0]
-
 		cur.execute(GET_USER_TWEETS.format(user_id))
 		total = 0
 		for dupe_tweet in cur.fetchall():
@@ -65,16 +78,12 @@ while True:
 		if count > 0:
 			val = float(total)/float(count)
 
-		print(UPDATE_USER.format(COL_NAME, val, user_id))
-		break
-		# cur.execute(UPDATE_USER.format(COL_NAME, val, user_id))
+		# print(UPDATE_USER.format(COL_NAME, val, user_id))
+		cur.execute(UPDATE_USER.format(COL_NAME, val, user_id))
 	cnx.commit()
 
-	i += 1
-	print("{}".format(i*BATCH_SIZE))
+	batch_stop = time.perf_counter()
+	time_remaining_est_min = (batch_stop-batch_start)*(total_users-users_processed)/60
+	print("{}/{} - {:.2f}".format(users_processed, total_users, time_remaining_est_min))
 
-# For each user
-	# Get count of number of tweets
-	# Get count of duplicate tweets
-		# So sum all the group-bys that have a count larger than 2.
-	# Save out ratio
+cnx.close()
